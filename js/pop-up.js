a647662e19c28ea70d7c0a5a6e94619fdb65a6ee
dmitry <kirildim16@gmail.com>
2019-08-22 12:46:03 +0300

    aded a popup

--- js/pop-up.js
+++ js/pop-up.js
@@ -5,6 +5,9 @@ document.addEventListener("DOMContentLoaded", e => {
     const pop_up_calc = document.querySelector('.pop-up-calc-wrap');
     const risk_management_button = document.querySelector('#risk-management');
     const pop_up_risk_management = document.querySelector('.pop-up-risk-management-wrap');
+    const pop_up_deawoo = document.querySelector('.pop-up-reorganization-of-daewoo-wrap');
+    const deawoo_reorg_button = document.querySelector('#deawoo-reorg');
+    const close_pop_up_reorg_deawoo = document.querySelector('.close-pop-up-reorganization-of-daewoo');
     
     risk_management_button.addEventListener('click', e => {
         pop_up_risk_management.classList.add('pop-up-show');
@@ -28,5 +31,14 @@ document.addEventListener("DOMContentLoaded", e => {
         pop_up_calc.classList.remove('pop-up-show');
     })
     
+    deawoo_reorg_button.addEventListener('click', e => {
+        pop_up_deawoo.classList.add('pop-up-show');
+    })
+    pop_up_deawoo.addEventListener('click', e => {
+        pop_up_deawoo.classList.remove('pop-up-show');
+    })
+    close_pop_up_reorg_deawoo.addEventListener('click', e => {
+        pop_up_deawoo.classList.remove('pop-up-show');
+    })
     
 });
