6a4c4ddd8a92f68128abb268f92b962c471dc8ee
dmitry <kirildim16@gmail.com>
2019-08-22 19:30:22 +0300

    is ready

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,9 @@
-/*$('.multiple-items').slick({
+
+$('.cases__cases-line').slick({
   infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});*/
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  nextArrow: '<div class="cases__btn_left"><i class="fa fa-chevron-left"></i></div>',
+  prevArrow: '<div class="cases__btn_right"><i class="fa fa-chevron-right"></i></div>'
+});
+
